85206d043d299eab3be4e7f5bafc34ec27dce829
Taylor Lineman <git@actuallytaylor.com>
2026-06-16 14:39:40 -0400

    Updated to impel versions
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

--- Package.swift
+++ Package.swift
@@ -27,18 +27,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FAISS",
-            url: "https://github.com/DeveloperMindset-com/faiss-mobile/releases/download/v1.7.4/faiss.xcframework.zip",
-            checksum: "85526c0384812d1b3c113dabb67ee779afe9b5225a9a57fbe936298a610b6f86"
+            url: "https://github.com/impel-intelligence/faiss-mobile/releases/download/v1.14.3/faiss.xcframework.zip",
+            checksum: "d102e5b46fe48cdd754870c16fe3a0f7220fba136b48c4febcdc30809fa8b7ec"
         ),
         .binaryTarget(
             name: "FAISS_C",
-            url: "https://github.com/DeveloperMindset-com/faiss-mobile/releases/download/v1.7.4/faiss_c.xcframework.zip",
-            checksum: "581024fc0495a42acf5305d96bb29fc98b6a23a4005ac495b0706a929c41c5a9"
+            url: "https://github.com/impel-intelligence/faiss-mobile/releases/download/v1.14.3/faiss_c.xcframework.zip",
+            checksum: "664e84f393929d5d0a10f164b3c2ea6b4636b604de68095e77f2fd08bdb32063"
         ),
         .binaryTarget(
             name: "OpenMP",
-            url: "https://github.com/DeveloperMindset-com/openmp-mobile/releases/download/v16.0.5/openmp.xcframework.zip",
-            checksum: "958970064322013ff4fa92ecb0ba53cea4eb3fce41be29498eb645e531f4fe6d"
+            url: "https://github.com/impel-intelligence/openmp-mobile/releases/download/v21.1.8/openmp.xcframework.zip",
+            checksum: "2f93a8273d648ab7b6d2c72c11a1684a8e63e0a1e6e6f2a326dae882d7acc20f"
         )
     ]
 )
